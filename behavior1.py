6062e82007ec09cc07ef5572bed81235b2f44774
Margaret <48628275+ElrondfromRussia@users.noreply.github.com>
2019-09-18 14:19:11 +0300

    Update behavior1.py

--- behavior1.py
+++ behavior1.py
@@ -2,5 +2,5 @@ import os
 import numpy
 
 def print_result(num):
-    print("I am heeere ", 5, " years and " , num, "month in ", os.curdir)
+    print("I am heeere ", 7, " years and " , num, "month in ", os.curdir)
 
