efdf0d7794b20cad7bedd5bf83ee8526703ea0af
Margaret <48628275+ElrondfromRussia@users.noreply.github.com>
2019-10-03 15:15:22 +0300

    Add files via upload

---
+++ behavior1.py
@@ -0,0 +1,11 @@
+from basilisk_agent import basilisk
+
+
+def begin_work(basil):
+    basil.show_help()
+    basil.show_help()
+    basil.show_help()
+    basil.show_help()
+    basil.start()
+    input("Press Enter")
+
